461371c389f46b3972680eb474d868008ce046f0
thevuong8000 <ducmanh.tran2904@gmail.com>
2022-03-05 18:14:07 -0500

    Initialize spring boot maven project

---
+++ backend/src/test/java/org/track1/clog/ClogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.track1.clog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
