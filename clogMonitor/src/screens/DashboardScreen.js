64ec8252eaeb370ac3c9d88103ada35ffeb2c55b
hieudan225 <hieudan225@gmail.com>
2022-03-07 23:07:05 -0500

    Add NameAndLogOut component

---
+++ clogMonitor/src/screens/DashboardScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NameAndLogout from '../components/NameAndLogout';
+
+function DashboardScreen(props) {
+    return (
+        <div>
+            <NameAndLogout />
+        </div>
+    );
+}
+
+export default DashboardScreen;
