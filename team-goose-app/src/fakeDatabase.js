e7c55a2018ef13daf253c2aa0440f2cf2420ce48
K Lin <emailklin@gmail.com>
2022-03-09 14:47:27 -0500

    Dropdowns get their options from the data

--- team-goose-app/src/fakeDatabase.js
+++ team-goose-app/src/fakeDatabase.js
@@ -26,3 +26,16 @@ export function getTableData(filters) {
     }
     return resultData;
 }
+
+export function getColumnValues(columnName) {
+    if(!columnName) {
+        return [""];
+    }
+    let dataCopy = [...data];
+    if(!dataCopy[0][columnName]) {
+        console.error("There is no column in the table with name: ", columnName);
+    }
+    let deDup = new Set();
+    dataCopy.forEach(row => deDup.add(row[columnName]));
+    return [...deDup];
+}
