8962552612838d8598b36e99a85684161699897e
Lo <hazarikalola@gmail.com>
2022-03-10 14:55:23 -0500

    Tree view updated

--- business-view-app/src/components/BusinessTree.js
+++ business-view-app/src/components/BusinessTree.js
@@ -1,60 +1,60 @@
 //this component is for the tree view where customer will be able to expand/contract a tree
 ///<!--<img src={"./shrink1.png"} />-->
+import './BusinessTree.css'
 
 const BusinessTree = () => {
+	const headingStyle = {
+		color: 'black', 
+		backgroundColor: 'lightblue',
+		alignitems: 'center'
+	}
+	
 	return (
-
-
-		<header>
+		<div>
 			<h1 style = {headingStyle}>Business Processes</h1>
 			<br /><br/><br/><br/><br/>
-			<p className="rectangle">
-			  &nbsp;&nbsp;&nbsp;
-			  <button>&nbsp;-&nbsp;</button>
-			  EAI Domain-1
-			  <br/>
-			  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-			  <button>&nbsp;-&nbsp;</button>
-			  Publishing Business Domain-1
-			  <br/>
-			  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-			  <button>&nbsp;-&nbsp;</button>
-			  Business Process-1
-			  <br/>
-			  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-			  <button>&nbsp;-&nbsp;</button>
-			  Business Process-1a BP Create Date; Context Name 1 = Context Value 1; Context Name 2 = Context Value 2
-			  <br/>
-			  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-			  <button>&nbsp;-&nbsp;</button>
-			  Business Process-1b BP Create Date; Context Name 1 = Context Value 1; Context Name 2 = Context Value 2
-			  <br/>
-			  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-			  <button>&nbsp;-&nbsp;</button>
-			  Business Process-1c BP Create Date; Context Name 1 = Context Value 1; Context Name 2 = Context Value 2
-			  <br/>
-			  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-			  <button>&nbsp;+&nbsp;</button>
-			  Business Process-2
-			  <br/>
-			  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-			  <button>&nbsp;+&nbsp;</button>
-			  Publishing Business Domain-2
-			  <br/>
-			  &nbsp;&nbsp;&nbsp;
-			  <button>&nbsp;-&nbsp;</button>
-			  EAI Domain-2
-			</p>
-		</header>
+			<div className="rectangle">
+				<div>
+				    <button>Expand All</button>
+				    <button>Contract All</button>
+				</div>
+			    <ul id="myUL">
+			  	    <li><span className="caret">EAI Domain-1</span>
+			  		    <ul className="nested">
+						    <li><span className="caret">Publishing Business Domain-1</span>
+							    <ul className="nested">
+								    <li><span className="caret">Business Process-1</span>
+									    <ul className="nested">
+										    <li>Business Process-1a BP Create Date; Context Name 1 = Context Value 1; Context Name 2 = Context Value2</li>
+										    <li>Business Process-1b BP Create Date; Context Name 1 = Context Value 1; Context Name 2 = Context Value2</li>
+										    <li>Business Process-1c BP Create Date; Context Name 1 = Context Value 1; Context Name 2 = Context Value2</li>
+									    </ul>
+								    </li>
+								    <li><span className= "caret">Business Process-2</span>
+								    </li>
+							    </ul>
+						    </li>
+						    <li><span className="caret">Publishing Business Domain-2</span>
+						    </li>
+					    </ul>
+			  	    </li>
+				    <li><span className="caret">EAI Domain-2</span>
+				    </li>
+			    </ul>
+			</div>
+		</div>
+		
 	)
 }
 
-const headingStyle = {
-	color: 'black', 
-	backgroundColor: 'lightblue',
-	alignitems: 'center'
-}
-
-
+    var toggler = document.getElementsByClassName("caret");
+	var i;
+	
+	for (i = 0; i < 30; i++) {
+	  toggler[i].addEventListener("click", function() {
+		this.parentElement.querySelector(".nested").classList.toggle("active");
+		this.classList.toggle("caret-down");
+	  });
+	} 
 
 export default BusinessTree
--- business-view-app/src/index.js
+++ business-view-app/src/index.js
@@ -2,24 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-/*const element = <h1 className = "testClass">Welcome</h1>;
-
-ReactDOM.render(element, document.getElementById("root"));
-
-const newElement = <h1 className = "testClass">Understanding</h1>;
-
-ReactDOM.render(newElement, document.getElementById("app"));*/
-
-/*const element = (
-    <div className = "testClass">
-        <h1>GIBBERISH</h1>
-        <h1>More Gibberish</h1>
-    </div>
-);
-
-ReactDOM.render(element, document.getElementById("root"));*/
+//import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <React.StrictMode>
