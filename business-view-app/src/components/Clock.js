68bf218675076f4e9d1a129d4f9aa31c74f38d20
Gabby Albrecht <galbrecht28@gmail.com>
2022-03-24 18:11:31 -0400

    New branch. Removed binary files (.vs/)

---
+++ business-view-app/src/components/Clock.js
@@ -0,0 +1,32 @@
+import './BusinessTree.css'
+import React, {useState, useEffect} from 'react';
+
+
+const Clock = () => {
+	const [dateState, setDateState] = useState(new Date());
+
+    useEffect(() => {setInterval(() => setDateState(new Date()), 30000)}, []);
+	
+	return (
+		<div className = 'imagePos'>
+			<img src='calender.png' width={30} height={30} alt='Calender' />
+            {' '}
+                {dateState.toLocaleDateString('en-GB', {
+                    day: 'numeric',
+                    month: 'short',
+                    year: 'numeric',
+                })}
+
+            <img src='clock.png' width={30} height={30}  alt='Large Pizza' />
+            {dateState.toLocaleString('en-US', {
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    hour12: true,
+                })}
+		</div>
+		
+	)
+}
+ 
+
+export default Clock
