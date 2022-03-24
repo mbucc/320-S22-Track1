1b37d6599781dfd7c0808a3bb682a38e3c3b6841
wilsonnexus <96637419+wilsonnexus@users.noreply.github.com>
2022-03-24 02:19:40 -0400

    New Branch Transfer
    New Branch Transfer

---
+++ business-view-app/src/App.js
@@ -0,0 +1,29 @@
+//import logo from './logo.svg';
+import React from 'react'
+import BusinessTree from './components/BusinessTree'
+import './App.css';
+
+
+
+const App = () => {
+
+  return (
+    <div className="container">
+      <BusinessTree/>
+    </div>
+    
+
+  )
+}
+
+
+/*class App extends React.Component {
+    render() {
+        return (
+        <Header />
+        //<h1>Hi</h1>
+      )
+    }
+}*/
+//export to index.js
+export default App;
