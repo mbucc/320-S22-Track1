a2473928ef060d67d329a13e6a4ba81d3834816f
Andrei-Treil <andrei.treil@gmail.com>
2022-03-29 19:44:29 -0400

    Combined all business log controller methods into one method

--- backend/src/main/java/org/track1/clog/repository/BusinessProcessLogRepository.java
+++ backend/src/main/java/org/track1/clog/repository/BusinessProcessLogRepository.java
@@ -1,12 +1,15 @@
 package org.track1.clog.repository;
 import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
+
+//import org.springframework.data.jpa.domain.Specification;
+//import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import org.track1.clog.model.BusinessProcessLog;
 import java.sql.Timestamp;
 
 @Repository
-public interface BusinessProcessLogRepository extends ReadOnlyRepository<BusinessProcessLog, String> {
+public interface BusinessProcessLogRepository extends ReadOnlyRepository<BusinessProcessLog, String>, JpaSpecificationExecutor<BusinessProcessLog> {
 
     List<BusinessProcessLog> findByEaiTransactionId(String eai_transaction_id);
     
@@ -29,4 +32,5 @@ public interface BusinessProcessLogRepository extends ReadOnlyRepository<Busines
     List<BusinessProcessLog> findByApplication(String application);
     
     List<BusinessProcessLog> findByActivity(String activity);
+
 }
