4bdd9a9f50e2c55ea6479e49f9b5f437a0300d0d
Junzhu Li <junzhuli1999@gmail.com>
2022-03-31 22:40:19 -0400

    Dropdown Cypress Tesing
    added dropdown testings

--- team-goose-app/src/components/Dropdown.js
+++ team-goose-app/src/components/Dropdown.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { FormControl, FormLabel, MenuItem, Select } from "@mui/material";
+//import { identity } from "cypress/types/lodash";
 
 /**
  * A wrapper for a MaterialUI Select with form control capability
@@ -8,17 +9,18 @@ import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
  * 
  * @param {Object} props
  * @param {string} props.label - A label for the dropdown
+ * @param {string} props.id - An id for the dropdown
  * @param {string} props.value - The current value for the selected option
  * @param {Array<string}} props.options - A list of option values for the dropdown
  * @param {(event: Event) => any} props.handleSelection - Handler for when an option is selected
  * 
  * @returns {React.ElementType}
  */
-const Dropdown = ({ label, value, options, handleSelection }) => {
+const Dropdown = ({ label, id, value, options, handleSelection }) => {
     return (
-        <FormControl margin="normal" style={{ width: "250px" }}>
-            <InputLabel>{label}</InputLabel>
-            <Select label={label} value={value} onChange={handleSelection}>
+        <FormControl  className={`dropdown-group ${label}`} margin="normal" style={{ width: "250px" }}>
+            <FormLabel className='dropdown-label'>{label}</FormLabel>
+            <Select id={id} label={label} value={value} onChange={handleSelection}>
                 <MenuItem value={"All"}>All</MenuItem>
                 {options.map((option) => (
                     <MenuItem key={option+"_id"} value={option}>{option}</MenuItem>
