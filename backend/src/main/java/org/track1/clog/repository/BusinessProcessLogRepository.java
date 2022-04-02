f50db543ae45218d33f8a0a215b96e2b289abd1c
Andrei-Treil <andrei.treil@gmail.com>
2022-04-02 13:20:57 -0400

    merged s6e-integrate

--- backend/src/main/java/org/track1/clog/repository/BusinessProcessLogRepository.java
+++ backend/src/main/java/org/track1/clog/repository/BusinessProcessLogRepository.java
@@ -10,27 +10,4 @@ import java.sql.Timestamp;
 
 @Repository
 public interface BusinessProcessLogRepository extends ReadOnlyRepository<BusinessProcessLog, String>, JpaSpecificationExecutor<BusinessProcessLog> {
-
-    List<BusinessProcessLog> findByEaiTransactionId(String eai_transaction_id);
-    
-    List<BusinessProcessLog> findByEaiDomain(String eai_domain);
-    
-    List<BusinessProcessLog> findByPublishingBusinessDomain(String publishing_business_domain);
-    
-    List<BusinessProcessLog> findByBusinessProcess(String business_process);
-
-    //find between two eai timestamps
-    List<BusinessProcessLog> findByEaiTransactionCreateTimeBetween(Timestamp eai_transaction_create_time_first, Timestamp eai_transaction_create_time_last);
-
-    //find after eai timestamp
-    List<BusinessProcessLog> findByEaiTransactionCreateTimeAfter(Timestamp eai_transaction_create_time);
-    
-    List<BusinessProcessLog> findByKey1AppContextNameAndKey1AppContextValue(String key1_app_context_name, String key1_app_context_value);
-
-    List<BusinessProcessLog> findByGlobalInstanceId(String global_instance_id);
-    
-    List<BusinessProcessLog> findByApplication(String application);
-    
-    List<BusinessProcessLog> findByActivity(String activity);
-
 }
