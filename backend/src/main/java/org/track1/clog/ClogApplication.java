8ef494d822a884847b2ade779b6c7563f3a569e6
Jaden Busch <jqdenbusch@gmail.com>
2022-04-13 17:48:07 -0400

    support for accessing swagger pages

--- backend/src/main/java/org/track1/clog/ClogApplication.java
+++ backend/src/main/java/org/track1/clog/ClogApplication.java
@@ -2,11 +2,15 @@ package org.track1.clog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import org.track1.clog.security.JWTAuthorizationFilter;
@@ -21,15 +25,33 @@ public class ClogApplication {
 	@EnableWebSecurity
 	@Configuration
 	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
+		
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
 			http.csrf().disable()
 				.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
 				.authorizeRequests()
 				.antMatchers(HttpMethod.POST, "/user").permitAll()
-				.anyRequest().authenticated();
+				.antMatchers("/swagger-ui/**").permitAll()
+				.anyRequest().authenticated()
+				.and()
+				.httpBasic();
 		}
+	
+		@Override
+		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+			auth.inMemoryAuthentication()
+					.withUser("admin")
+					.password(passwordEncoder().encode("admin"))
+					.authorities("ADMIN");
+		}
+	
+		@Bean
+		public PasswordEncoder passwordEncoder() {
+			return new BCryptPasswordEncoder();
+		}
+	
+
 	}
 
 }
