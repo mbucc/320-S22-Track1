ac4d6f1ad7004de10fc3d63b4bf9fcaf4d7dc6fa
Junzhu Li <junzhuli1999@gmail.com>
2022-04-13 09:44:08 -0400

    updated dropdown test

--- clogMonitor/cypress/integration/DropdownTest.spec.js
+++ clogMonitor/cypress/integration/DropdownTest.spec.js
@@ -1,4 +1,5 @@
-/// <reference types="cypress" />
+//<reference types="cypress" />
+import {getColumnValues} from '../../src/fakeDatabase'
 
 describe('Dropdown works correctly', () => {
 
@@ -30,128 +31,93 @@ describe('Dropdown works correctly', () => {
       cy.get('.dropdown-group > .dropdown-label').contains("Process/Service").should("exist");
     });
 
+    // Dropdown states
+    const EAIDomains = getColumnValues("EAI_DOMAIN");
+    const businessDomains = getColumnValues("BUSINESS_DOMAIN");
+    const businessSubDomains = getColumnValues("BUSINESS_SUBDOMAIN")
+    const applications = getColumnValues("APPLICATION");
+    const processIds = getColumnValues("EVENT_CONTEXT");
+
     it('displays all options', () => {
       // We use the `click()` command to click a DOM element
 
       // EAI Domain
       cy.get('[id="EAI_DOMAIN_ID"]').click();
-      cy.get('[role="listbox"]').children().contains("All");
-      cy.get('[role="listbox"]').children().contains("EAI_DOMAIN_1");
+      for (let i = 0; i < EAIDomains.length; i++){
+        cy.get('[role="listbox"]').children().contains(EAIDomains[i]);
+      }
       // unclick our selections
       cy.get('body').click(0, 0);
 
       // Business Domain
       cy.get('[id="BUSINESS_DOMAIN_ID"]').click();
-      cy.get('[role="listbox"]').children().contains("All");
-      cy.get('[role="listbox"]').children().contains("CRM");
-      cy.get('[role="listbox"]').children().contains("OPER");
-      cy.get('[role="listbox"]').children().contains("ACCOUNT");
+      for (let i = 0; i < businessDomains.length; i++){
+        cy.get('[role="listbox"]').children().contains(businessDomains[i]);
+      }
       cy.get('body').click(0, 0);
 
       // Business Subdomain
       cy.get('[id="BUSINESS_SUBDOMAIN_ID"]').click();
-      cy.get('[role="listbox"]').children().contains("All");
-      cy.get('[role="listbox"]').children().contains("Customer");
+      for (let i = 0; i < businessSubDomains.length; i++){
+        cy.get('[role="listbox"]').children().contains(businessSubDomains[i]);
+      }
       cy.get('body').click(0, 0);
 
       // Application
       cy.get('[id="APPLICATION_ID"]').click();
-      cy.get('[role="listbox"]').children().contains("All");
-      cy.get('[role="listbox"]').children().contains("CRM_Adapter");
-      cy.get('[role="listbox"]').children().contains("OPER_Adapter");
-      cy.get('[role="listbox"]').children().contains("ACCOUNT_Adapter");
+      for (let i = 0; i < applications.length; i++){
+        cy.get('[role="listbox"]').children().contains(applications[i]);
+      }
       cy.get('body').click(0, 0);
 
       // PROCESS_SERVICE
-      cy.get('[id="PROCESS_SERVICE_ID"]').click();
-      cy.get('[role="listbox"]').children().contains("All");
-      cy.get('[role="listbox"]').children().contains("Publish_Customer_Update");
-      cy.get('[role="listbox"]').children().contains("Customer_Update");
+      cy.get('[id="PROCESS_SERVICE_ID"]').click(); 
+      for (let i = 0; i < processIds.length; i++){
+        cy.get('[role="listbox"]').children().contains(processIds[i]);
+      }
       cy.get('body').click(0, 0);
     });
 
     it('selects an option by clicking', () => {
 
       // EAI Domain
-      cy.get('[id="EAI_DOMAIN_ID"]').click();
-      cy.get('[role="listbox"]').children().contains("All").click();
-      cy.get('[id="EAI_DOMAIN_ID"]').contains("All");
-      cy.get('body').click(0, 0);
-
-      cy.get('[id="EAI_DOMAIN_ID"]').click();
-      cy.get('[role="listbox"]').children().contains("EAI_DOMAIN_1").click();
-      cy.get('[id="EAI_DOMAIN_ID"]').contains("EAI_DOMAIN_1");
-      cy.get('body').click(0, 0);
+      for (let i = 0; i < EAIDomains.length; i++){
+        cy.get('[id="EAI_DOMAIN_ID"]').click();
+        cy.get('[role="listbox"]').children().contains(EAIDomains[i]).click();
+        cy.get('[id="EAI_DOMAIN_ID"]').contains(EAIDomains[i]);
+        cy.get('body').click(0, 0);      
+      }
 
       // Business Domain
-      cy.get('[id="BUSINESS_DOMAIN_ID"]').click();
-      cy.get('[role="listbox"]').children().contains("All").click();
-      cy.get('[id="BUSINESS_DOMAIN_ID"]').contains("All");
-      cy.get('body').click(0, 0);
-
-      cy.get('[id="BUSINESS_DOMAIN_ID"]').click();
-      cy.get('[role="listbox"]').children().contains("CRM").click({force: true});
-      cy.get('[id="BUSINESS_DOMAIN_ID"]').contains("CRM");
-      cy.get('body').click(0, 0);
-      
-      cy.get('[id="BUSINESS_DOMAIN_ID"]').click();
-      cy.get('[role="listbox"]').children().contains("OPER").click({force: true});
-      cy.get('[id="BUSINESS_DOMAIN_ID"]').contains("OPER");
-      cy.get('[role="listbox"]').children().contains("OPER").click({force: true});
-      cy.get('body').click(0, 0);
-
-      cy.get('[id="BUSINESS_DOMAIN_ID"]').click();
-      cy.get('[role="listbox"]').children().contains("ACCOUNT").click({force: true});
-      cy.get('[id="BUSINESS_DOMAIN_ID"]').contains("ACCOUNT");
-      cy.get('[role="listbox"]').children().contains("ACCOUNT").click({force: true});
-      cy.get('body').click(0, 0);
+      for (let i = 0; i < businessDomains.length; i++){
+        cy.get('[id="BUSINESS_DOMAIN_ID"]').click();
+        cy.get('[role="listbox"]').children().contains(businessDomains[i]).click();
+        cy.get('[id="BUSINESS_DOMAIN_ID"]').contains(businessDomains[i]);
+        cy.get('body').click(0, 0);      
+      }
 
       // Business Subdomain
-      cy.get('[id="BUSINESS_SUBDOMAIN_ID"]').click();
-      cy.get('[role="listbox"]').children().contains("All").click();
-      cy.get('[id="BUSINESS_SUBDOMAIN_ID"]').contains("All");
-      cy.get('body').click(0, 0);
-
-      cy.get('[id="BUSINESS_SUBDOMAIN_ID"]').click();
-      cy.get('[role="listbox"]').children().contains("Customer").click();
-      cy.get('[id="BUSINESS_SUBDOMAIN_ID"]').contains("Customer");
-      cy.get('body').click(0, 0);
+      for (let i = 0; i < businessSubDomains.length; i++){
+        cy.get('[id="BUSINESS_SUBDOMAIN_ID"]').click();
+        cy.get('[role="listbox"]').children().contains(businessSubDomains[i]).click();
+        cy.get('[id="BUSINESS_SUBDOMAIN_ID"]').contains(businessSubDomains[i]);
+        cy.get('body').click(0, 0);      
+      }
 
       // Application
-      cy.get('[id="APPLICATION_ID"]').click();
-      cy.get('[role="listbox"]').children().contains("All").click();
-      cy.get('[id="APPLICATION_ID"]').contains("All");
-      cy.get('body').click(0, 0);
-
-      cy.get('[id="APPLICATION_ID"]').click();
-      cy.get('[role="listbox"]').children().contains("CRM_Adapter").click();
-      cy.get('[id="APPLICATION_ID"]').contains("CRM_Adapter");
-      cy.get('body').click(0, 0);
-
-      cy.get('[id="APPLICATION_ID"]').click();
-      cy.get('[role="listbox"]').children().contains("OPER_Adapter").click();
-      cy.get('[id="APPLICATION_ID"]').contains("OPER_Adapter");
-      cy.get('body').click(0, 0);
-
-      cy.get('[id="APPLICATION_ID"]').click();
-      cy.get('[role="listbox"]').children().contains("ACCOUNT_Adapter").click();
-      cy.get('[id="APPLICATION_ID"]').contains("ACCOUNT_Adapter");
-      cy.get('body').click(0, 0);
+      for (let i = 0; i < applications.length; i++){
+        cy.get('[id="APPLICATION_ID"]').click();
+        cy.get('[role="listbox"]').children().contains(applications[i]).click();
+        cy.get('[id="APPLICATION_ID"]').contains(applications[i]);
+        cy.get('body').click(0, 0);      
+      }
 
       // PROCESS_SERVICE
-      cy.get('[id="PROCESS_SERVICE_ID"]').click();
-      cy.get('[role="listbox"]').children().contains("All").click();
-      cy.get('[id="PROCESS_SERVICE_ID"]').contains("All");
-      cy.get('body').click(0, 0);
-
-      cy.get('[id="PROCESS_SERVICE_ID"]').click();
-      cy.get('[role="listbox"]').children().contains("Publish_Customer_Update").click();
-      cy.get('[id="PROCESS_SERVICE_ID"]').contains("Publish_Customer_Update");
-      cy.get('body').click(0, 0);
-
-      cy.get('[id="PROCESS_SERVICE_ID"]').click();
-      cy.get('[role="listbox"]').children().contains("Customer_Update").click();
-      cy.get('[id="PROCESS_SERVICE_ID"]').contains("Customer_Update");
-      cy.get('body').click(0, 0);
+      for (let i = 0; i < processIds.length; i++){
+        cy.get('[id="PROCESS_SERVICE_ID"]').click();
+        cy.get('[role="listbox"]').children().contains(processIds[i]).click();
+        cy.get('[id="PROCESS_SERVICE_ID"]').contains(processIds[i]);
+        cy.get('body').click(0, 0);      
+      }
     });
 });
