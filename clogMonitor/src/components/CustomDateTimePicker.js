c19e3d149cbcab45112992415b6ca38e5742c782
advaitsharma7 <advait268742@gmail.com>
2022-04-13 13:51:38 -0400

    resolved errors

--- clogMonitor/src/components/CustomDateTimePicker.js
+++ clogMonitor/src/components/CustomDateTimePicker.js
@@ -1,42 +1,42 @@
-import * as React from 'react';
-import AlarmIcon from '@mui/icons-material/Alarm';
-import SnoozeIcon from '@mui/icons-material/Snooze';
-import TextField from '@mui/material/TextField';
-import ClockIcon from '@mui/icons-material/AccessTime';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import DateTimePicker from '@mui/lab/DateTimePicker';
-import MobileDateTimePicker from '@mui/lab/MobileDateTimePicker';
-import Stack from '@mui/material/Stack';
+import * as React from "react";
+import AlarmIcon from "@mui/icons-material/Alarm";
+import SnoozeIcon from "@mui/icons-material/Snooze";
+import TextField from "@mui/material/TextField";
+import ClockIcon from "@mui/icons-material/AccessTime";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import DateTimePicker from "@mui/lab/DateTimePicker";
+import MobileDateTimePicker from "@mui/lab/MobileDateTimePicker";
+import Stack from "@mui/material/Stack";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import Background from './Background.js';
+// import Background from './Background.js';
 
 /* Author: @wilsonnexus, isRangeError() from Team Goose
-* Customizable MUI Date Time Range Picker with Error Handling
-* Still needs some more work, but I think the time values could be 
-* sent across files now. Military Time To Do
-*/
+ * Customizable MUI Date Time Range Picker with Error Handling
+ * Still needs some more work, but I think the time values could be
+ * sent across files now. Military Time To Do
+ */
 const themeLight = createTheme({
-    palette: {
-      background: {
-        default: "#000000"
-      },
-      text: {
-        primary: "#FFFFFF"
-      }
-    }
-  });
-  
+  palette: {
+    background: {
+      default: "#000000",
+    },
+    text: {
+      primary: "#FFFFFF",
+    },
+  },
+});
+
 const themeDark = createTheme({
-    palette: {
-      background: {
-        default: "#FFFFFF"
-      },
-      text: {
-        primary: "#000000"
-      }
-    }
-  });
+  palette: {
+    background: {
+      default: "#FFFFFF",
+    },
+    text: {
+      primary: "#000000",
+    },
+  },
+});
 
 export default function CustomDateTimePicker() {
   const [clearedDate, setClearedDate] = React.useState(null);
@@ -44,49 +44,47 @@ export default function CustomDateTimePicker() {
   const [endTime, setEndTime] = React.useState(new Date());
 
   const isRangeError = () => {
-        // Convert times to actual Date objects to compare
-        // Note: we only care about the relative difference, so time zone should not matter
-        const dt_start = new Date(startTime);
-        const dt_end =new Date(endTime);
-        return dt_end < dt_start;
-    }
+    // Convert times to actual Date objects to compare
+    // Note: we only care about the relative difference, so time zone should not matter
+    const dt_start = new Date(startTime);
+    const dt_end = new Date(endTime);
+    return dt_end < dt_start;
+  };
 
   return (
-  <ThemeProvider theme={Background.light ? themeLight : themeDark}>
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <Stack spacing={3} sx={{ width: 0.9}} sy={{ height: 2}} >
-        
-        <DateTimePicker 
-          showTodayButton
-          value={startTime}
-          onChange={(newValue) => {
-            setStartTime(newValue);
-          }}
-          ampm={false}
-          label="Start Time"
-          onError={console.log}
-          maxDate={endTime}
-          inputFormat="dd/MM/yyyy HH:mm"
-          mask="___/__/__ __:__"
-          renderInput={(params) => <TextField {...params} />}
-        />
-        <DateTimePicker
-        showTodayButton
-          value={endTime}
-          onChange={(newValue) => {
-            setEndTime(newValue);
-          }}
-          ampm={false}
-          label="End Time"
-          onError={console.log}
-          minDate={startTime}
-          inputFormat="dd/MM/yyyy HH:mm"
-          mask="___/__/__ __:__"
-          renderInput={(params) => <TextField {...params} />}
-        />
-        
-      </Stack>
-    </LocalizationProvider>
+    <ThemeProvider theme={themeLight}>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <Stack spacing={3} sx={{ width: 0.9 }} sy={{ height: 2 }}>
+          <DateTimePicker
+            showTodayButton
+            value={startTime}
+            onChange={(newValue) => {
+              setStartTime(newValue);
+            }}
+            ampm={false}
+            label="Start Time"
+            onError={console.log}
+            maxDate={endTime}
+            inputFormat="dd/MM/yyyy HH:mm"
+            mask="___/__/__ __:__"
+            renderInput={(params) => <TextField {...params} />}
+          />
+          <DateTimePicker
+            showTodayButton
+            value={endTime}
+            onChange={(newValue) => {
+              setEndTime(newValue);
+            }}
+            ampm={false}
+            label="End Time"
+            onError={console.log}
+            minDate={startTime}
+            inputFormat="dd/MM/yyyy HH:mm"
+            mask="___/__/__ __:__"
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </Stack>
+      </LocalizationProvider>
     </ThemeProvider>
   );
 }
