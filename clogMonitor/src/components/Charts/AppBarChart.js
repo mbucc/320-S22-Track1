c2d25a5b1875c38615f8ecee63295f250f3b12f8
hieudan225 <hieudan225@gmail.com>
2022-04-13 13:47:14 -0400

    Attempt to add time span selection for Graph

--- clogMonitor/src/components/Charts/AppBarChart.js
+++ clogMonitor/src/components/Charts/AppBarChart.js
@@ -4,7 +4,7 @@ import Stack from "@mui/material/Stack";
 import Box from '@mui/material/Box';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { Select, MenuItem } from "@mui/material";
-  
+
 const errorData = [
     {
         name: '1/1-1/7',
@@ -58,30 +58,31 @@ const warningData = [
     },
 ];
 
-export default function AppBarChart() {
+export default function AppBarChart({ logEvents }) {
     const [chart, setChart] = useState("Warning");
+    console.log(logEvents)
     return (
         <div>
             <Typography component='div'>
-                <Box sx={{ 
-                    textAlign: 'left', 
+                <Box sx={{
+                    textAlign: 'left',
                     m: 3,
                     fontWeight: 'bold',
                     fontFamily: 'Monospace',
-                    fontSize: 'h4.fontSize',                    
-                    }}
-                  > Bar Chart 
-                  <Select>
-                  <MenuItem value = "Error Log" onClick={() => setChart("Error Log")}> Error Log Events </MenuItem>
-                  <MenuItem value = "Warning Log" onClick={() => setChart("Warning")}> Warning Log Events </MenuItem>
-                </Select>
-                </Box>                
+                    fontSize: 'h4.fontSize',
+                }}
+                > Bar Chart
+                    <Select>
+                        <MenuItem value="Error Log" onClick={() => setChart("Error Log")}> Error Log Events </MenuItem>
+                        <MenuItem value="Warning Log" onClick={() => setChart("Warning")}> Warning Log Events </MenuItem>
+                    </Select>
+                </Box>
             </Typography>
             <Stack
                 direction="row"
                 spacing={1}
                 sx={{
-                    display: 'flex', 
+                    display: 'flex',
                     justifyContent: 'center',
                     bgcolor: 'background.paper',
                     position: "relative",
@@ -92,44 +93,44 @@ export default function AppBarChart() {
                     boxShadow: "0px 0px 12px -1px #000000"
                 }}
             >
-    
+
                 {chart === "Error Log" ? (
-                <div>
-                    <BarChart width={470} height={470} data={errorData}>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Bar dataKey="application_2" stackId="a" fill="#8884D8" />
-                    <Bar dataKey="application_3" stackId="a" fill="#82CA9D" />
-                    <Bar dataKey="application_1" stackId="a" fill="#FFC658" />
-                    </BarChart>
-                    <Typography variant="heading1">
-                        The number of error log events per application
-                    </Typography>
-                </div>
+                    <div>
+                        <BarChart width={470} height={470} data={errorData}>
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Bar dataKey="application_2" stackId="a" fill="#8884D8" />
+                            <Bar dataKey="application_3" stackId="a" fill="#82CA9D" />
+                            <Bar dataKey="application_1" stackId="a" fill="#FFC658" />
+                        </BarChart>
+                        <Typography variant="heading1">
+                            The number of error log events per application
+                        </Typography>
+                    </div>
                 ) : (
-                <></>
+                    <></>
                 )}
                 {chart === "Warning" ? (
                     <div>
-                    <BarChart width={470} height={470} data={warningData}>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Bar dataKey="application_2" stackId="a" fill="#60F542" />
-                    <Bar dataKey="application_3" stackId="a" fill="#82CA9D" />
-                    <Bar dataKey="application_1" stackId="a" fill="#F5425D" />
-                    </BarChart>
-                    <Typography variant="heading1">
-                        The number of warning log events per application
-                    </Typography>
-                </div>
+                        <BarChart width={470} height={470} data={warningData}>
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Bar dataKey="application_2" stackId="a" fill="#60F542" />
+                            <Bar dataKey="application_3" stackId="a" fill="#82CA9D" />
+                            <Bar dataKey="application_1" stackId="a" fill="#F5425D" />
+                        </BarChart>
+                        <Typography variant="heading1">
+                            The number of warning log events per application
+                        </Typography>
+                    </div>
                 ) : (
-                <></>
+                    <></>
                 )}
             </Stack>
         </div>
