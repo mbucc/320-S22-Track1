08580f6cb3c07b67ca7c098132fb264ad7538135
isaacmillg <igebrewolde@gmail.com>
2022-04-19 20:39:21 -0400

    added direction prop to CheckboxGroup

--- clogMonitor/src/components/CheckboxGroup.js
+++ clogMonitor/src/components/CheckboxGroup.js
@@ -11,10 +11,11 @@ import { Checkbox, FormControl, FormControlLabel, FormGroup, FormHelperText, For
  * @param {Set<string>} props.selectedOptions - The set of currently selected options
  * @param {Array<string>} props.options - A list of possible option values (in the order of appearance)
  * @param {(event: Event) => any} props.handleSelection - Handler for when an option is selected
+ * @param {string} props.direction - Direction of checkboxes ('row' or 'column'). Direction is row by default
  * 
  * @returns {React.ElementType}
  */
-const CheckboxGroup = ({label, options, selectedOptions, handleSelection}) => {
+const CheckboxGroup = ({label, options, selectedOptions, handleSelection, direction}) => {
     // The group has an error if there are no selected options
     const isError = () => {
         return selectedOptions.size < 1;
@@ -23,7 +24,7 @@ const CheckboxGroup = ({label, options, selectedOptions, handleSelection}) => {
     return (
         <FormControl className={`checkbox-group ${label}`} error={isError()} style={{minWidth: "115px"}}>
             <FormLabel className='checkbox-label'>{label}</FormLabel>
-            <FormGroup>
+            <FormGroup row={direction === 'column' ? true : false}>
                 {options.map(option => 
                     <FormControlLabel
                         className='checkbox'
