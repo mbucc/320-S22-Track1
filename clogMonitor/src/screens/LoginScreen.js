34e7d797d09550d0b0d3e396b015979cb53be005
K Lin <emailklin@gmail.com>
2022-04-20 21:05:48 -0400

    Rewrite element styling into class styling

--- clogMonitor/src/screens/LoginScreen.js
+++ clogMonitor/src/screens/LoginScreen.js
@@ -71,12 +71,12 @@ function LoginScreen({ setLoggedIn }) {
       <form onSubmit={handleSubmit}>
         <div className="input-container">
           <label>Username </label>
-          <input type="text" name="uname" required />
+          <input className="outlined-input" type="text" name="uname" required />
           {renderErrorMessage("uname")}
         </div>
         <div className="input-container">
           <label>Password </label>
-          <input type="password" name="pass" required />
+          <input className="outlined-input" type="password" name="pass" required />
           {renderErrorMessage("pass")}
         </div>
         <div className="button-container">
