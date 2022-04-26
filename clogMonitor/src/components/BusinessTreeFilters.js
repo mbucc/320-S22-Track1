9ba7a6fc62459e1190dcddcc8d7174e13727e9a4
hiimlo <hazarikalola@gmail.com>
2022-04-25 20:07:34 -0400

    updating the files based on customer documents, in process of reworking components

--- clogMonitor/src/components/BusinessTreeFilters.js
+++ clogMonitor/src/components/BusinessTreeFilters.js
@@ -1,28 +1,16 @@
 import React from "react";
 import {Button, FormControl} from "@mui/material";
-import CustomDateTimePicker from "../components/CustomDateTimePicker"
+import CustomDateTimePicker from "./CustomDateTimePicker"
 import { Grid} from '@mui/material';
 import Dropdown from "./Dropdown";
-import BusinessTree from '../components/BusinessTree';
+import BusinessTree from './BusinessTree';
 
 
 /**
- * Note: for this to work the way we want it to, we use a  "form"
- * Components that should be put here (as of 3/31 understanding):
- *   Start/End time
- *   EAI domain drop down
- *   Publishing domain drop down
- *   tree view
- *   Apply button
- * 
- * --> Refresh button does not need to be here since it simply updates the times
- * Essentially how this will be built is within some sort of Form tag (<FormControl />)
- * these components should be added
- * All components listed are "inputs", as well as apply button (exception: this "submits" the form)
- * Handlers for each component need to exist so we can keep track of what has been selected. 
- * Keeping track is important so that when we apply(submit form), filters are known
- * 
- * https://www.pluralsight.com/guides/form-submission-in-reactjs <-- resource to understand
+ * AS OF 4/25
+ * Time and dropdowns affect the TREE, not the table
+ * APPLY BUTTON DOES NOT NEED TO BE HERE
+ * Selection from tree populates the table
  * 
  * - @hiimlo note
  * 
