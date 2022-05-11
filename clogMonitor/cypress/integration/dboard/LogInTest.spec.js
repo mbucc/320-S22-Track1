965fb9d9827f3daf17edf2f8db9ce62ae9d34286
hieudan225 <hieudan225@gmail.com>
2022-05-10 22:42:13 -0400

    Add more Login tests

--- clogMonitor/cypress/integration/dboard/LogInTest.spec.js
+++ clogMonitor/cypress/integration/dboard/LogInTest.spec.js
@@ -7,6 +7,22 @@ describe("LoginTests", () => {
         cy.get(".app").should("exist");
     });
 
+    it("test if forgot password is clicked", () => {
+        cy.get('p').click().get(".error").should("exist");
+    });
+
+    it("test if the error message show up if both the username and password is left empty", () => {
+        cy.get(':nth-child(1) > .outlined-input').type('{enter}').get(':nth-child(2) > .outlined-input').type('{enter}').get(".error").should("exist");
+    });
+
+    it("test if the error message show up if the username is left empty", () => {
+        cy.get(':nth-child(1) > .outlined-input').type('{enter}').get(':nth-child(2) > .outlined-input').type('teamkick{enter}').get(".error").should("exist");
+    });
+
+    it("test if the error message show up if the password is left empty", () => {
+        cy.get(':nth-child(1) > .outlined-input').type('root{enter}').get(':nth-child(2) > .outlined-input').type('{enter}').get(".error").should("exist");
+    });
+
     it("test the correct credentials", () => {
         cy.get(':nth-child(1) > .outlined-input').type('root{enter}').get(':nth-child(2) > .outlined-input').type('teamkick{enter}').get(".DashboardScreen").should("exist");
     });
