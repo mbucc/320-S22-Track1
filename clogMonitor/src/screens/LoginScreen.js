09d9858f37283d79cd88f6ee6d4a7132a7f9dfda
hieudan225 <hieudan225@gmail.com>
2022-05-10 23:02:07 -0400

    Add login tests

--- clogMonitor/src/screens/LoginScreen.js
+++ clogMonitor/src/screens/LoginScreen.js
@@ -22,15 +22,15 @@ function LoginScreen({ setLoggedIn }) {
     var { uname, pass } = document.forms[0];
     // Find user login info
     validateCredential(uname.value, pass.value)
-    .then(token => {
-      sessionStorage.setItem("token", token);
-      sessionStorage.setItem("loginCheck", "true");
-      setTimeout(() => setLoggedIn("true"), 100); // Wait until session storage is set before logging in
-    }).catch(error => {
-      console.log(error);
-      setErrorMessages({ name: "uname", message: errors.uname });
-      setErrorMessages({ name: "pass", message: errors.pass });
-    });
+      .then(token => {
+        sessionStorage.setItem("token", token);
+        sessionStorage.setItem("loginCheck", "true");
+        setTimeout(() => setLoggedIn("true"), 100); // Wait until session storage is set before logging in
+      }).catch(error => {
+        console.log(error);
+        setErrorMessages({ name: "uname", message: errors.uname });
+        setErrorMessages({ name: "pass", message: errors.pass });
+      });
   }
 
   // Bypass login screen for log details in new tab
@@ -38,7 +38,7 @@ function LoginScreen({ setLoggedIn }) {
     if (window.location.href.indexOf('log-details') !== -1) {
       setLoggedIn("true")
     }
-  },[]);
+  }, []);
 
 
   function help() {
@@ -51,29 +51,29 @@ function LoginScreen({ setLoggedIn }) {
 
   // Generate JSX code for error message
   const renderErrorMessage = (name) =>
-    name === errorMessages.name && (
-      <div className="error">{errorMessages.message}</div>
-    );
+  (
+    <div className="error">{errorMessages.message}</div>
+  );
 
   // JSX code for login form
   const renderForm = (
-      <div className="form">
-        <form onSubmit={handleSubmit}>
-          <div className="input-container">
-            <label>Username </label>
-            <input className="outlined-input username-input" type="text" name="uname" required />
-            {renderErrorMessage("uname")}
-          </div>
-          <div className="input-container">
-            <label>Password </label>
-            <input className="outlined-input password-input" type="password" name="pass" required />
-            {renderErrorMessage("pass")}
-          </div>
-          <div className="button-container">
-            <input className="login-btn" type="submit" />
-          </div>
-        </form>
-        <br></br>
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label>Username </label>
+          <input className="outlined-input username-input" type="text" name="uname" required />
+          {renderErrorMessage("uname")}
+        </div>
+        <div className="input-container">
+          <label>Password </label>
+          <input className="outlined-input password-input" type="password" name="pass" required />
+          {renderErrorMessage("pass")}
+        </div>
+        <div className="button-container">
+          <input className="login-btn" type="submit" />
+        </div>
+      </form>
+      <br></br>
       <div className="help-button">
         <p onClick={help}> Forgot Password? </p>
       </div>
@@ -82,8 +82,8 @@ function LoginScreen({ setLoggedIn }) {
   return (
     <div className="app">
       <div className="login-form">
-        <Grid container justify = "center">
-          <img src = {ISOLogo} alt="sunset on a beach"/>
+        <Grid container justify="center">
+          <img src={ISOLogo} alt="sunset on a beach" />
         </Grid>
         <div className="title"> CLOG Monitor Sign In</div>
         {isSubmitted ? (
